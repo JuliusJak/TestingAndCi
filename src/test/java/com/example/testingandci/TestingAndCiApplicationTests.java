6a84e0cbef4120a91885db0f89ce8bd7825a04d9
JuliusJak <julius.bis03@gmail.com>
2023-09-20 15:23:50 +0200

    added test for savedAccount

--- src/test/java/com/example/testingandci/TestingAndCiApplicationTests.java
+++ src/test/java/com/example/testingandci/TestingAndCiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.testingandci;
 
+import com.example.testingandci.model.Account;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,9 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.example.testingandci.repository.IAccountRepository;
 import com.example.testingandci.service.AccountService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+//@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class TestingAndCiApplicationTests {
 
@@ -32,5 +34,34 @@ class TestingAndCiApplicationTests {
 		accountService.deleteAccount(accountIdToDelete);
 
 		verify(repository, times(1)).deleteById(accountIdToDelete);
+	}
+	@Test
+	public void saveAccount(){
+		Account mockAccount = Account.builder()
+				.id(1L)
+				.username("John Doe")
+				.contactInfo("112")
+				.accountType("ADMIN")
+				.paymentInfo(112)
+				.build();
+
+		when(repository.save(any(Account.class))).thenReturn(mockAccount);
+
+		Account newAccount = Account.builder()
+				.username("Jane")
+				.contactInfo("112")
+				.accountType("ADMIN")
+				.paymentInfo(112)
+				.build();
+
+		Account savedAccount = accountService.saveAccount(newAccount);
+
+		verify(repository, times(1)).save(newAccount);
+
+		assertEquals(mockAccount, savedAccount);
+		System.out.println(newAccount);
+		System.out.println(mockAccount);
+		System.out.println(savedAccount);
+
 	}
 }
