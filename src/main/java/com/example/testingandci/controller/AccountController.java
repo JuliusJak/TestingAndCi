2f6648254143058a629f9a4b923e69aeb2bdac96
JuliusJak <julius.bis03@gmail.com>
2023-09-25 15:33:29 +0200

    done with basic tests for AccountService

--- src/main/java/com/example/testingandci/controller/AccountController.java
+++ src/main/java/com/example/testingandci/controller/AccountController.java
@@ -2,18 +2,11 @@ package com.example.testingandci.controller;
 
 import com.example.testingandci.model.Account;
 import com.example.testingandci.service.AccountService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.AccountNotFoundException;
-
-import static org.springframework.util.ObjectUtils.isEmpty;
 
 @RestController
 @RequestMapping("account/")
--- src/test/java/com/example/testingandci/TestingAndCiApplicationTests.java
+++ src/test/java/com/example/testingandci/TestingAndCiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.testingandci;
 
+import com.example.testingandci.controller.AccountController;
 import com.example.testingandci.model.Account;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,8 +10,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.example.testingandci.repository.IAccountRepository;
 import com.example.testingandci.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 //@SpringBootTest
@@ -27,6 +30,11 @@ class TestingAndCiApplicationTests {
 	@InjectMocks
 	private AccountService accountService;
 
+	@InjectMocks
+	private AccountController accountController;
+
+
+	// AccountService Tests
 	@Test
 	public void testDeleteAccount() {
 		Long accountIdToDelete = 1L;
@@ -59,9 +67,66 @@ class TestingAndCiApplicationTests {
 		verify(repository, times(1)).save(newAccount);
 
 		assertEquals(mockAccount, savedAccount);
-		System.out.println(newAccount);
-		System.out.println(mockAccount);
-		System.out.println(savedAccount);
+	}
+	@Test
+	public void testFetchedAccount() {
+
+		Long accountId = 1L;
+		Account mockAccount = new Account();
+		mockAccount.setUsername("tests");
+
+		//Account exists
+		when(repository.findById(accountId)).thenReturn(java.util.Optional.of(mockAccount));
+
+		Account result = accountService.fetchedAccount(accountId);
+
+
+		assertNotNull(result);
+		assertEquals(mockAccount, result);
+		System.out.println(result);
+
+		verify(repository, times(1)).findById(accountId);
+
+
+
+		//Account does not exist
+		when(repository.findById(accountId)).thenReturn(java.util.Optional.empty());
+
+		result = accountService.fetchedAccount(accountId);
+
+		assertNull(result);
+		System.out.println(result);
+		verify(repository, times(2)).findById(accountId);
 
 	}
+
+	@Test
+	public void testUpdateAccount() {
+
+		Account oldAccount = Account.builder()
+				.id(1L)
+				.username("John Doe")
+				.contactInfo("112")
+				.accountType("ADMIN")
+				.paymentInfo(112)
+				.build();
+
+		System.out.println("old account");
+		System.out.println(oldAccount);
+		when(repository.save(oldAccount)).thenReturn(oldAccount);
+
+		Account updatedAccount = accountService.updateAccount(oldAccount);
+
+		System.out.println("new account");
+		System.out.println(updatedAccount);
+
+		assertNotNull(updatedAccount);
+		assertEquals(oldAccount, updatedAccount);
+
+		verify(repository, times(1)).save(oldAccount);
+	}
+
+
+	//AccountController Tests
+
 }
